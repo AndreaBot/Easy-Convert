d5a828d8775110c49a5d647e222f0d8fae7efb91
Andrea Bottino <bottinoandrea93@gmail.com>
2024-03-28 21:12:10 +0000

    Implemented MVVM

--- Easy Convert/ContentView.swift
+++ Easy Convert/ContentView.swift
@@ -9,25 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     
-    private let units = [
-        ["-", "km", "m", "cm", "mm", "mi", "yd", "ft", "in"],
-        ["-", "kg", "g", "mg", "lb", "oz"],
-        ["-", "°C", "K", "°F"]
-    ]
-    
-    @State private var valueToConvert = 0.0
-    @State private var unitTag = 0
-    @State private var startUnit = "-"
-    @State private var targetUnit = "-"
+    @State private var viewModel = ViewModel()
     @FocusState private var valueIsFocused: Bool
-    
-    private var convertedValue : Double {
-        if unitTag == 2 {
-            return UnitConversions.convertTemperature(value: valueToConvert, startUnit: startUnit, targetUnit: targetUnit)
-        } else {
-            return UnitConversions.convert(value: valueToConvert, startUnit: startUnit, targetUnit: targetUnit)
-        }
-    }
     
     var body: some View {
         NavigationStack {
@@ -37,37 +20,39 @@ struct ContentView: View {
                 
                 VStack(spacing: 0) {
                     
-                    Picker("Types", selection: $unitTag) {
+                    Picker("Types", selection: $viewModel.unitTag) {
                         Text("Length").tag(0)
                         Text("Weight").tag(1)
                         Text("Temperature").tag(2)
                     }
                     .padding()
                     .pickerStyle(.segmented)
-                    .onChange(of: unitTag) {
-                        startUnit = "-"
-                        targetUnit = "-"
-                        valueToConvert = 0
+                    .onChange(of: viewModel.unitTag) {
+                        viewModel.startUnit = "-"
+                        viewModel.targetUnit = "-"
+                        viewModel.valueToConvert = 0
                     }
                     Form {
                         HStack {
-                            Button("", systemImage: "arrow.up.arrow.down") {
-                                let first = startUnit
-                                let second = targetUnit
-                                startUnit = second
-                                targetUnit = first
+                            Button {
+                                let first = viewModel.startUnit
+                                let second = viewModel.targetUnit
+                                viewModel.startUnit = second
+                                viewModel.targetUnit = first
+                            } label: {
+                                Image(systemName: "arrow.up.arrow.down")
                             }
                             .frame(maxWidth: 40)
                             .buttonStyle(PlainButtonStyle())
                             
                             VStack {
-                                Picker("From", selection: $startUnit) {
-                                    ForEach(units[unitTag], id: \.self) {
+                                Picker("From", selection: $viewModel.startUnit) {
+                                    ForEach(viewModel.units[viewModel.unitTag], id: \.self) {
                                         Text($0).tag($0)
                                     }
                                 }
-                                Picker("To", selection: $targetUnit) {
-                                    ForEach(units[unitTag], id: \.self) {
+                                Picker("To", selection: $viewModel.targetUnit) {
+                                    ForEach(viewModel.units[viewModel.unitTag], id: \.self) {
                                         Text($0).tag($0)
                                     }
                                 }
@@ -76,10 +61,10 @@ struct ContentView: View {
                         
                         Section {
                             HStack {
-                                TextField("Amount to convert", value: $valueToConvert, format: .number)
+                                TextField("Amount to convert", value: $viewModel.valueToConvert, format: .number)
                                     .keyboardType(.decimalPad)
                                     .focused($valueIsFocused)
-                                Text(UnitConversions.convertSymbols(symbol: startUnit))
+                                Text(viewModel.convertSymbols(symbol: viewModel.startUnit))
                             }
                         } header: {
                             Text("Enter value to convert")
@@ -87,9 +72,9 @@ struct ContentView: View {
                         
                         Section {
                             HStack {
-                                Text(String(format: "%.3f", convertedValue))
+                                Text(String(format: "%.3f", viewModel.convertedValue))
                                 Spacer()
-                                Text(UnitConversions.convertSymbols(symbol: targetUnit))
+                                Text(viewModel.convertSymbols(symbol: viewModel.targetUnit))
                             }
                         } header: {
                             Text("Converted value:")
